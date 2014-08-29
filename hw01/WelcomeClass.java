4e3b621ab97f092e9b5713af5698b8ec915d6491
suzychen <zic217@lehigh.edu>
2014-08-29 15:29:35 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+//Zijia Chen
+//zic217
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//  print the welcome message with a box around it
+    System.out.println("   -----------   ");
+    System.out.println("   | WELCOME |   ");
+    System.out.println("   -----------   ");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-Z--I--C--2--1--7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This May my parents and I went to Thailand and enjoyed wonderful elephant ride, Thai massage and tropical fruits.");
+    
+    //test/testtest
+    
+    
+    }
+}
