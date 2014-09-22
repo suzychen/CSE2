e7651c98310d68b8045aacc721d188ca678f0c5a
suzychen <zic217@lehigh.edu>
2014-09-22 19:26:40 +0000

    updated TimePadding

--- hw04/TimePadding.java
+++ hw04/TimePadding.java
@@ -25,26 +25,35 @@ public class TimePadding{
         System.out.print("Enter the number in seconds: ");
         //initialize the number as a integer and give the value from scanner to the number
         int number=myScanner.nextInt();
-        int hours=(int)number/3600;
-        int minutes=(int)((number-(hours*3600))/60);
-        int secs=(int)(number-(hours*3600)-(minutes*60));
+        int hours=(int)number/3600; //initialize hour and compute hours from the seconds entered
+        int minutes=(int)((number-(hours*3600))/60); //initialize minute and compute minutes from the seconds entered
+        int secs=(int)(number-(hours*3600)-(minutes*60)); //initialize second and compute seconds from the seconds entered
         
+        //set the condition that the seconds entered is a positive number
         if  (number > 0){
+            //set the condition that is minutes and seconds are one-decimal numbers
             if (minutes < 10 && secs < 10){
+            //print out the padded time form with 0 in front of minutes and seconds
             System.out.println("The time is "+hours+":"+"0"+minutes+":"+"0"+secs);
         }
+            //set the condition that if minutes is a one-decimal number and second is a two-decimal number
             else if (minutes < 10 && secs > 10){
+            //print out the padded time form with 0 in front of minutes
             System.out.println("The time is "+hours+":"+"0"+minutes+":"+secs);
             }
+            //set the condition that if minutes is a two-decimal number and seconds is a one-decimal number
             else if (minutes > 10 && secs < 10){
+            //print out the padded time form with 0 in front of seconds
             System.out.println("The time is "+hours+":"+minutes+":"+"0"+secs);
             }
+            //the condition that both minutes and seconds are two-decimal numbers
             else{
             System.out.println("The time is "+hours+":"+minutes+":"+secs);
             }
         }
+        //the conditin that the number entered is not a positive number
         else {
-            System.out.println("The number entered is positibe.");
+            System.out.println("The number entered is not positive.");
         }
         
     }
